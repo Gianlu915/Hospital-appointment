1ce6777174149bb292e58478f32dd419ab1cbef8
Gianluca Liccardo <gianluca_liccardo@libero.it>
2024-10-29 15:39:14 +0700

    Add new appointment form with two-way data binding

---
+++ src/app/models/appointment.model.ts
@@ -0,0 +1,11 @@
+export interface Appointment {
+    name: string;
+    mobileNo: string;
+    city: string;
+    age: number;
+    gender: string;
+    appointmentDate: string;
+    appointmentTime: string;
+    isFirstVisit: boolean;
+    narration: string;
+  }
--- src/app/pages/new-appointment/new-appointment.component.ts
+++ src/app/pages/new-appointment/new-appointment.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppointmentListComponent } from '../appointment-list/appointment-list.component';
+import { Appointment } from '../../models/appointment.model';
 
 @Component({
   selector: 'app-new-appointment',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class NewAppointmentComponent {
 
+  appointmentObj: Appointment = { // Inizializza appointmentObj con un oggetto che rispetta l'interfaccia Appointment
+    name: '',
+    mobileNo: '',
+    city: '',
+    age: 0,
+    gender: '',
+    appointmentDate: '',
+    appointmentTime: '',
+    isFirstVisit: true,
+    narration: ''
+  };
 }
